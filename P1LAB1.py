42393ef488fa183e23daed06914040c5450fa0f0
thomasm-school <thomasm3142@student.faytechcc.edu>
2025-10-02 12:54:37 -0400

    Add files via upload

---
+++ P1LAB1.py
@@ -0,0 +1,11 @@
+
+# Matthew Thomas
+# October 2nd, 2025
+# P1LAB1
+# The goal is to get input from the user and display output.
+
+print("Enter your first name: ")
+firstname=input()
+print("Enter your last name: ")
+lastname=input()
+print("Hello", firstname, lastname, "and welcome to CIT110!" )
